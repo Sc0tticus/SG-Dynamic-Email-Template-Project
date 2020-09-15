6920bfe840ea99c16e38a2dbea89ec7ea8b1be5d
Sc0tticus <ssinger303@gmail.com>
2020-09-14 21:55:11 -0600

    Created a Unit Test for a dynamic email template

--- index.js
+++ index.js
@@ -1,7 +1,27 @@
-if ('text' in conflictingData && 'dynamicTemplateData' in conflictingData) {
-  console.log('text and dynamicTemplateData cannot both have values');
-} else if ('html' in conflictingData && 'dynamicTemplate' in conflictingData) {
-  console.log('html and dynamicTemplateData cannot both have values');
-} else {
-  console.log('The email can be sent without issue.');
+const conflictingData = {
+  to: 'recipient@example.org',
+  from: 'sender@example.org',
+  subject: 'Hello world',
+  text: 'Hello plain world!',
+  dynamicTemplateData: 'Some fancy template data'
+}
+
+function dynamicTemplateUnitTest(email){
+  const message = 'This email can be sent';
+try {
+  if ('text' in email && 'dynamicTemplateData' in email) 
+  throw 'text and dynamicTemplateData fields cannot both have values';
+  if ('html' in email && 'dynamicTemplateData' in email)
+  throw 'html and dynamicTemplateData fields cannot both have values';
+  }
+finally {c
+  return message;
+  }
+}
+
+console.log(test(conflictingData))
+
+function adder(a, b){
+  c = a + b ;
+  return c
 }
